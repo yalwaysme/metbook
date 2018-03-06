05ea7344fdff3c4c5e888e37e268cbb7df240715
PRussell116 <UP806925@uni.ds.port.ac.uk>
2018-03-06 13:53:44 +0000

    Added function addrating

---
+++ backend/config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const path = require('path');
+
+// mysql
+module.exports.mysql = {
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  charset: 'UTF8MB4',
+  database: 'metBookDB', // change to real name
+
+};
---
+++ backend/server.js
@@ -0,0 +1,47 @@
+'use static'
+
+const express = require ('express');
+const mysql = require('mysql2/promise');
+
+
+
+const config = require('./config');
+
+const sqlPromise = mysql.createConnection(config.mysql);
+
+
+// add DB
+const app = express();
+
+// listen for requests
+app.listen(8080, (err) => {
+  if (err)
+    console.error('error starting server', err);
+  else
+    console.log('server started');
+  }
+);
+
+
+
+async function pullBooks(req,res){
+  console.log("");
+}
+
+
+async function addRating(req,res){
+  const newRating ={
+    userId: req.query.userId
+    score: req.query.score
+    description: req.query.desc
+  }
+  const [rows] = await sql.query(sql.format('INSERT INTO Reviews SET ?', newRating)); // maybe rename Reviews
+  return { id: rows.insertId, title: newRating.userId,score: newRating.score,description: newRating.description };
+}
+
+
+
+
+module.exports.add = {
+  addRating : addRating
+}
